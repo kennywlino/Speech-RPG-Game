d632e4042bfe84c2ab71c3b3eb520c58d38d46f9
Kenny Lino <kennywlino@gmail.com>
2017-05-13 18:42:28 +0200

    Update to a work version; missing appropriate comments to describe functions.

--- src/Game/player.py
+++ src/Game/player.py
@@ -4,7 +4,7 @@ Features such as inventories are currently defined but are not used in the curre
 of the game.
 '''
 
-import items, world, speech
+import items, world, speech, feedback
 import random
 
 class Player():
@@ -42,16 +42,25 @@ class Player():
     # this is the module that requires checking the speech to text to the enemies sentences
     # Make the change to the player that they lose HP if they say the sentence wrong HERE
     def attack(self, enemy):
-        user_text = speech.recognize_speech()
-        if user_text == # what the enemies sentence is
-            damage = random.randint(3, enemy.hp - 1)
-            if damage == enemy.hp:
-                print("WOAH! That was critical!")
+        print(enemy.sentence)
+        user_text = (speech.recognize_speech()).lower()
+        if user_text == enemy.sentence.lower():
+            damage = enemy.hp
             enemy.hp -= damage
-        if not enemy.is_alive():
             print("You killed {}!".format(enemy.name))
         else:
-            print("{} HP is {}.",format(enemy.name, enemy.hp))
+            self.hp -= enemy.damage
+            print("Enemy does {} damage. You have {} HP remaining.".format(enemy.damage, self.hp))
+            option=input("Oops, looks like you need to improve your communication skills. The fairies of the forest come in your help. 'Hello wanderer! You have 3 different ways to get feedback, choose an option so we can perform our magic!'\n1. Get the arpabet transcription\n2. Get the IPA transcription\n3. Get some descriptive advice\n\n")
+            while option not in '123':
+                option=input("Try again with 1, 2, or 3 please.\n\n")
+            if option=="1":
+                print(feedback.arpabet(enemy.sentence.rstrip('.')))
+            elif option=="2":
+                print(feedback.ipa(enemy.sentence))
+            elif option=="3":
+                ipa_sentence = feedback.ipa(enemy.sentence)
+                feedback.minimal_pairs_advice(enemy.sentence, ipa_sentence)
             
     def do_action(self, action, **kwargs):
         action_method = getattr(self, action.method.__name__)
